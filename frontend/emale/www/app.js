0dbdce8b8de3f893dd353ad75362c365befb319b
nadav <nadav22@gmail.com>
2014-09-16 22:34:19 +0300

    addiing firebase

--- frontend/emale/www/app.js
+++ frontend/emale/www/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('emale', ['ionic','emale.services'])
+angular.module('emale', ['ionic','emale.services','firebase'])
     .config(function($stateProvider,$urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
         $stateProvider
--- frontend/emale/www/main_list/main_list.js
+++ frontend/emale/www/main_list/main_list.js
@@ -3,21 +3,32 @@
  */
 
 (function(){
-    function mainListController(scope,dataService){
+    function mainListController(scope,dataService,firebase){
 
-        scope.fullList = dataService.getFullList();
+        var ref = new Firebase("https://glaring-fire-7037.firebaseio.com/");
 
+        // create an AngularFire reference to the data
+        var sync = firebase(ref);
 
-//      todo : fix ctrl as so we can use thr this.fullList->
-//        this.fullList = function(){
-//            return dataService.getFullList();
-//        };
+        // download the data into a local object
+        var syncObject = sync.$asObject();
+
+        // synchronize the object with a three-way data binding
+        syncObject.$bindTo(scope, "data");
+
+        // create a synchronized array for use in our HTML code
+        scope.messages = sync.$asArray();;
+
+        scope.addMessage = function(text) {
+            scope.messages.$add({text: text});
+        }
+
 
 
 
     };
 
     angular.module('emale')
-        .controller('mainListController',['$scope','dataService',mainListController]);
+        .controller('mainListController',['$scope','dataService',"$firebase",mainListController]);
 }());
 
